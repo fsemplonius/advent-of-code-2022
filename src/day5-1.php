031dca5023771839cbdac6ec66304150977ed261
Folkert Semplonius <folkert@semplonius.info>
2022-12-05 11:54:20 +0100

    Added day 5

---
+++ src/day5-1.php
@@ -0,0 +1,32 @@
+<?php
+
+include 'day5PuzzleInput.php';
+$input = explode("\r\n", $s1);
+
+$lines = $stacks = array();
+
+while (($s1=next($input))[1] != '1')
+  array_unshift($lines, $s1);
+
+$n = 1;
+while ($s1[$n]) {
+  $stack[$s1[$n]] = array ();
+  foreach ($lines as $line)
+    if ($line[$n] != ' ') $stacks[$s1[$n]][] = $line[$n];
+  $n += 4;
+}
+
+next($input);
+while ($s2=next($input)) {
+  list ($dum1, $n, $dum2, $fr, $dum3, $to) = explode(' ', $s2);
+  for ($i=0; $i<$n; $i++)
+    $stacks[$to][] = array_pop($stacks[$fr]);
+}
+
+$result = '';
+foreach ($stacks as $stack)
+  $result .= end($stack);
+
+echo "Result: $result";
+
+?>
